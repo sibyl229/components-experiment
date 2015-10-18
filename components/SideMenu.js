b3bdb10ad709da2e1c272b609daea91cef731b7c
Sibyl <sibyl@wormbase.org>
2015-10-17 21:28:29 -0400

    very crude animation with CSSTransitionGroup

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Card, CardHeader, CardMedia, CardTitle, CardText, CardActions, FlatButton } from 'material-ui';
 import { getImageUrl } from '../utils';
 import MenuList from './MenuList';
-//import styles from './SideMenu.css';
+import styles from './SideMenu.css';
 
+let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 export default class SideMenu extends Component {
   constructor(props) {
@@ -40,21 +41,26 @@ export default class SideMenu extends Component {
 
     return (
       <div className="Grid" style={{...style,...this.props.style}} >
-        <div className="GridCell">
+
+        <div key="a" className="GridCell">
           <MenuList menuID="primary"
+                    key="primary"
                     level={0}
                     onSubmenuRequest={this.showMenuList}
                     onSubmenuCancel={this.hideMenuList}/>
         </div>
-        <div className="GridCell">
+        <div key="b" className="GridCell">
+        <ReactCSSTransitionGroup transitionName="example">
         {
           console.log(this.state) || this.state[1]
-            ? <MenuList menuID={this.state[1]}
+            ? <MenuList key="m" menuID={this.state[1]}
+                        key={this.state[1]}
                         level={1}
                         onSubmenuRequest={this.showMenuList}
                         onSubmenuCancel={this.hideMenuList}/>
             : null
         }
+        </ReactCSSTransitionGroup>
         </div>
       </div>);
   }
